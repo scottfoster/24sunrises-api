fea8cfe838e0fe1e63d401f1ec7fd3e14ed59a72
Scott Foster <scott@imfoster.com>
2023-04-29 06:41:58 -0500

    caching getutc

--- getutc.php
+++ getutc.php
@@ -5,10 +5,10 @@ serverless bref:local -f getutc --data='{"queryStringParameters": {"latitude":"2
 */
 
 require 'models/database.php';
-require 'models/sunrises.php';
+require 'models/offsets.php';
 
 use Models\Database;
-use Models\Sunrises;
+use Models\Offsets;
 
 new Database();
 
@@ -18,13 +18,26 @@ return function ($event)
     $latitude = (float)$event['queryStringParameters']['latitude'];
     $longitude = (float)$event['queryStringParameters']['longitude'];
 
-    $time = file_get_contents('https://vip.timezonedb.com/v2.1/get-time-zone?key=SQ90W55WY9V6&format=json&by=position&lat='.$latitude.'&lng='.$longitude);
-    $time = json_decode($time);
+    $latitude = round($latitude, 2);
+    $longitude = round($longitude, 2);
 
-    $timezone = new DateTimeZone($time->zoneName);
-    $time = new DateTime('now', $timezone);
-    $offset = round($timezone->getOffset($time)/3600);
-    if($offset > 0){ $offset = '+'.$offset; }
+    $offset = Offsets::where('latitude', $latitude)->where('longitude', $longitude)->value('offset');
+    if(!$offset)
+    {
+        $time = file_get_contents('https://vip.timezonedb.com/v2.1/get-time-zone?key=SQ90W55WY9V6&format=json&by=position&lat='.$latitude.'&lng='.$longitude);
+        $time = json_decode($time);
+
+        $timezone = new DateTimeZone($time->zoneName);
+        $time = new DateTime('now', $timezone);
+        $offset = round($timezone->getOffset($time)/3600);
+        if($offset > 0){ $offset = '+'.$offset; }
+
+        $offsets = new Offsets();
+        $offsets->latitude = $latitude;
+        $offsets->longitude = $longitude;
+        $offsets->offset = $offset;
+        $offsets->save();
+    }
 
     return json_encode(['offset'=>$offset]);
 };
